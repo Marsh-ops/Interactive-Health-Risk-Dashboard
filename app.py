8651b566f1eca136957ec3344e2598cebfbe592d
Marsh-ops <mh_rossi@hotmail.com>
2025-12-21 12:32:42 +1030

    Update

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 import json
-from google.oauth2 import service_account
 
 #---------------------------------------------------------------------------------------------#
 # --- Load trained models ---
