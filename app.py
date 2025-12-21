0f9ab426b18c241c87fd4db15a308c0763d6867b
Marsh-ops <mh_rossi@hotmail.com>
2025-12-21 23:48:43 +1030

    Update to pdf form

--- app.py
+++ app.py
@@ -463,7 +463,7 @@ if results:
     st.bar_chart(risk_df.set_index('Disease'))
 
 #---------------------------------------------------------------------------------------------#
-# --- PDF Generation ---
+# --- PDF Generation and Export ---
 #---------------------------------------------------------------------------------------------#
 
 # Disease descriptions
@@ -505,14 +505,11 @@ def save_pdf(results_dict, patient_info):
     # Disease sections
     for disease, risk in results_dict.items():
 
-            # --- Divider line above each disease ---
+        # --- Divider line above each disease ---
         y = pdf.get_y()
         pdf.set_draw_color(180, 180, 180)  # light grey
         pdf.line(10, y, 200, y)
         pdf.ln(4)
-
-        pdf.set_font("Arial", 'B', 14)
-        pdf.cell(200, 8, txt=f"{disease} Risk: {risk*100:.1f}%", ln=True)
 
         pdf.set_font("Arial", 'B', 14)
         pdf.cell(200, 8, txt=f"{disease} Risk: {risk*100:.1f}%", ln=True)
